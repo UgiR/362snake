981ee11ea7a5219df01eb3a26c95e9ba615ebc81
UgiR <33397860+UgiR@users.noreply.github.com>
2019-04-17 13:29:20 -0500

    simple documentation

--- uint4.h
+++ uint4.h
@@ -5,6 +5,17 @@
 #ifndef INC_362SNAKE_UINT4_H
 #define INC_362SNAKE_UINT4_H
 
+/**
+ * uint4 - 4-bit unsigned integer
+ * Operator functionality may be incomplete!!
+ *
+ * Purpose for snake:
+ * A 4-bit integer will simplify the implementation of at least one snake feature.
+ *
+ * For example, when the snake encounters the end of the grid, it should "wrap around" and continue on the other side
+ * of the grid. A 4-bit integer will naturally overflow and will reset to 0, so there will be no need to implement
+ * this behaviour manually.
+ */
 class uint4 {
 private:
     unsigned int i : 4;
